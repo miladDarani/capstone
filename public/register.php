1291928a43549ba50da7209d69c3a08976c9652c
Milad Darani <57505176+miladDarani@users.noreply.github.com>
2020-04-25 13:38:58 -0500

    added all labels and styled the form

--- public/register.php
+++ public/register.php
@@ -22,67 +22,74 @@ require __DIR__ . "/../includes/header_inc.php";
 
 
         <!-- First name & Last name -->
-        <div class="fl_name">
-
-            <div class="f_name">
-            <input name="first_name" type="text" placeholder="First Name *" required value="" />
-          </div>
-
-            <div class="l_name">
-                <input name="last_name" type="text" placeholder="Last Name *" required value="" />
-            </div>
         
-        </div>
+    <div id="form_wrapper">
+        <p>  
+            <label for="first_name">First Name</label><br />
+            <input name="first_name" type="text" placeholder="First Name *" required value="" />
+         
+        </p>
+        <p>  
+            <label for="last_name">Last Name</label><br />
+            <input name="last_name" type="text" placeholder="Last Name *" required value="" />
+        </p>
+        
+      
         
         
          <p>
-          <input name="street" id="street" type="text" placeholder="Type your street address *" required value="" />
+            <label for="street">Street</label><br />
+            <input name="street" id="street" type="text" placeholder="Type your street address *" required value="" />
         </p>
 
 
          <p>
+            <label for="city">City</label><br />
           <input name="city" id="city" type="text" placeholder="City *" required value="" />
         </p>
 
         <p>
+            <label for="postal_code">Postal Code</label><br />
             <input name="postal_code" type="text" placeholder="Postal Code i.e R2J1L5 or 90210 *" required value="" />
         </p>
 
         <p>
+          <label for="province">Province</label><br />  
           <input name="province" id="province" type="text" placeholder="Province or State i.e ON or NY *" required value="" />
         </p>
 
          <p>
+          <label for="country">Country</label><br />
           <input name="country" id="country" type="text" placeholder="Country *" required value="" />
         </p>
 
         <p>
+            <label for="phone">Phone</label><br />
             <input name="phone" type="text" placeholder="Phone Number *" required value="" />
         </p>
 
         <p>
+          <label for="email">E-Mail</label><br />
           <input name="email" id="email" type="text" placeholder="Type your e-mail address *" required value="" />
         </p>
 
-         <p>
+        <p>
+          <label for="email">Confirm E-Mail</label><br />
           <input name="email2" id="email2" type="text" placeholder="Type your e-mail address again *" required value="" />
         </p>
 
-          <p>
+        <p>
+          <label for="password">Password</label><br />
           <input name="password" id="password" type="text" placeholder="Type your Password *" required value="" />
         </p>
 
-         <p>
+        <p>
+          <label for="email">Confirm Password</label><br />
           <input name="password2" id="password2" type="text" placeholder="Type your Password again *" required value="" />
         </p>
 
-        
-
-        
-
-       
-
         <p>
+            <label for="age">Age</label><br />
             <input name="age" type="text" placeholder="Your Age"  value="" />
         </p>
 
@@ -92,6 +99,7 @@ require __DIR__ . "/../includes/header_inc.php";
         </div>
 
     </form> <!-- end form -->
+</div>
 </div><!-- /wrapper -->
 
 
